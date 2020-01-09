3e0c4bf6ea2e52db4eb0f3c6737e81c0ba045f24
Michelle Sokolov <michellersokolov@gmail.com>
2020-01-09 12:09:30 -0800

    Add files via upload

---
+++ 13hw/public/assets/js/cats.js
@@ -0,0 +1,53 @@
+$(function() {
+    $(".change-sleep").on("click", function(event) {
+      var id = $(this).data("id");
+      var newSleep = $(this).data("newsleep");
+  
+      var newSleepState = {
+        sleepy: newSleep
+      };
+  
+      $.ajax("/api/cats/" + id, {
+        type: "PUT",
+        data: newSleepState
+      }).then(
+        function() {
+          console.log("changed sleep to", newSleep);
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      event.preventDefault();
+  
+      var newCat = {
+        name: $("#ca").val().trim(),
+        sleepy: $("[name=sleepy]:checked").val().trim()
+      };
+  
+      $.ajax("/api/cats", {
+        type: "POST",
+        data: newCat
+      }).then(
+        function() {
+          console.log("created new cat");
+          location.reload();
+        }
+      );
+    });
+  
+    $(".delete-cat").on("click", function(event) {
+      var id = $(this).data("id");
+  
+      $.ajax("/api/cats/" + id, {
+        type: "DELETE"
+      }).then(
+        function() {
+          console.log("deleted cat", id);
+          location.reload();
+        }
+      );
+    });
+  });
+  
